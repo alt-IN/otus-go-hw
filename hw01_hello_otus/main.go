8f9c69fe86b874924ebd4d3ffa8e2ccb321a505e
NikitaD <nikita135@gmail.com>
2024-09-09 23:27:46 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/hello/reverse"
+)
+
+const (
+	inputText = "Hello, OTUS!"
+)
+
 func main() {
 	// Place your code here.
+
+	fmt.Println(reverse.String(inputText))
 }
